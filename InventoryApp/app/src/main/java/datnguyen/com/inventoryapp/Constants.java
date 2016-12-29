dae78318d72eec2177778d50bccdd2ec9a2da1c3
Dat Nguyen <dat@healint.com>
2016-12-29 10:41:41 +0800

    - clean code

--- InventoryApp/app/src/main/java/datnguyen/com/inventoryapp/Constants.java
+++ InventoryApp/app/src/main/java/datnguyen/com/inventoryapp/Constants.java
@@ -5,12 +5,9 @@ package datnguyen.com.inventoryapp;
  */
 
 public class Constants {
-
-	public final static String EXTRA_PRODUCT_KEY = "product";
 	public static final int INVALID_ID = -1;
-
 	public static final int INVALID_INT_VALUE = Integer.MAX_VALUE;
 
+	public final static String EXTRA_PRODUCT_KEY = "product";
 	public final static String EXTRA_UPDATE_PRODUCT_RESULT_KEY = "EXTRA_UPDATE_PRODUCT_RESULT_KEY";
-
 }
